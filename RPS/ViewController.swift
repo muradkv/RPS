df2325d51ffe8105eefd9ef4cd42e6a8d1d34afa
murad <muradkv@gmail.com>
2019-07-12 00:01:07 +0300

    Add IBOutlets and Actions

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var sign: UILabel!
+    @IBOutlet var statusGame: UILabel!
+    @IBOutlet var rock: UIButton!
+    @IBOutlet var scissors: UIButton!
+    @IBOutlet var paper: UIButton!
+    @IBOutlet var playAgain: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rockTapped(_ sender: UIButton) {
+    }
+    @IBAction func scissorsTapped(_ sender: UIButton) {
+    }
+    @IBAction func paperTapped(_ sender: UIButton) {
+    }
+    @IBAction func playAgainTapped(_ sender: UIButton) {
+    }
 }
 
